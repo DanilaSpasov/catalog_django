36f2a4edbf56c8a38a47f151ee2053fed210feca
Danila Spasov <spasov2000@mail.ru>
2026-06-29 23:08:47 +0300

    Add product_details.html and update README.md.

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product
 
 
 def home(request):
     product_list = Product.objects.all()
-    context = {'product_list': product_list}
+    context = {"product_list": product_list}
     return render(request, "home.html", context)
 
 
 def contacts(request):
     return render(request, "contacts.html")
+
+
+def product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, "product_details.html", {"product": product})
