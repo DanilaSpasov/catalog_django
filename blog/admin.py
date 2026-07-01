e64655d164b97d472a1ab332bd1c6c6edb90d579
Danila Spasov <spasov2000@mail.ru>
2026-07-01 21:27:19 +0300

    add new app Blog, update README.md

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from blog.models import Blog
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created_at", "is_published")
+    list_display_links = ("title",)
+    search_fields = ("title", "content")
+    list_filter = ("is_published",)
+
+
