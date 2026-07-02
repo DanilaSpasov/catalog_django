987382e0b3f1be9aad1c7cca52bc23274fd2f42a
Danila Spasov <spasov2000@mail.ru>
2026-07-03 01:12:31 +0300

    add CRUD method classes in Catalog for Products. Add forms for create and update.

---
+++ catalog/forms.py
@@ -0,0 +1,64 @@
+from django import forms
+
+from catalog.models import Product
+
+BANNED_WORDS = [
+    "казино",
+    "криптовалюта",
+    "крипта",
+    "биржа",
+    "дешево",
+    "дёшево",
+    "бесплатно",
+    "обман",
+    "полиция",
+    "радар",
+]
+
+class ProductUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ["title", "description", "image", "category", "price"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["title"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Введите название"
+        })
+
+        self.fields["description"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Введите описание"
+        })
+
+        self.fields["image"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Загрузите изображение"
+        })
+
+        self.fields["category"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Введите категорию"
+        })
+
+        self.fields["price"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Введите цену"
+        })
+
+    def clean(self):
+        cleaned_data = super().clean()
+        lower_title = cleaned_data.get("title", "").lower()
+        lower_description = self.cleaned_data("description", "").lower()
+        for word in BANNED_WORDS:
+            if word in lower_title or word in lower_description:
+                raise forms.ValidationError(f"Запрещено использовать слово {word} в заголовке или описании.")
+        return cleaned_data
+
+    def clean_price(self):
+        price = self.cleaned_data["price"]
+        if price < 0 and price is not None:
+            raise forms.ValidationError("Цена не может быть отрицательной.")
+        return price
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from catalog.models import Product
 
@@ -21,3 +21,22 @@ class ProductDetailsView(DetailView):
     template_name = "product_details.html"
     context_object_name = "product"
     pk_url_kwarg = "pk"
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = "product_create.html"
+    fields = ["title", "description", "price", "image"]
+    success_url = "/catalog/"
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = "product_update.html"
+    fields = ["title", "description", "price", "image"]
+    pk_url_kwarg = "pk"
+    success_url = "/catalog/"
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = "product_delete.html"
+    pk_url_kwarg = "pk"
+    success_url = "/catalog/"
